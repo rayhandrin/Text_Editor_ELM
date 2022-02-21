15674e3cf5a7647462e39f40891c8eb88109f99b
rof000 <91263023+rof000@users.noreply.github.com>
2022-02-21 14:56:06 +0700

    quitAndSave.cpp

--- quitAndSave.cpp
+++ quitAndSave.cpp
@@ -5,11 +5,16 @@
 void quitAndSave(char text1[10])
 {
 	char copyText[10];
+	
+	//Membuka dan membuat file dengan format file txt
 	FILE *text = fopen("text.txt", "a");
 	
+	//Meng copy kan text yang sudah di inputkan ke dalam variabel copyText bertipe char 
 	strcpy(copyText,text1);
 	
+	//Memasukan text kedalam file 
 	fprintf(text," %s",copyText);
 	
+	//Menutup file
 	fclose(text);
 }

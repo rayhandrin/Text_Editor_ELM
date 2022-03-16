48639be1103d77920cd8c9c24f57072cce9dbc2f
Rofi Fauzan <91263023+RofiFauzanAl@users.noreply.github.com>
2022-03-16 21:24:12 +0700

    Add files via upload

---
+++ headerMenu.hpp
@@ -0,0 +1,10 @@
+#ifndef headerMenu_hpp
+#define headerMenu_hpp
+
+/* Deklarasi untuk modul menuInterface.cpp */
+void optionMenu();
+
+/* Deklarasi untuk modul quitProgram.cpp */
+void quitProgram();
+
+#endif
